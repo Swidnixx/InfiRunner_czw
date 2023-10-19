e3383b56a47473eb68e3c37e75e815b032887127
Swidnixx <swidnixx@gmail.com>
2023-10-19 18:24:06 +0200

    FInished jumping and slow fall

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public LayerMask groundMask; 
     public float jumpForce = 2;
+    public float pushForce = 200;
     Rigidbody2D rb;
+    BoxCollider2D boxCollider;
+    bool grounded; //Player on the ground
+    bool doubleJumped; // Player already jumped twice
+    bool jumpHeld; // Used to know if player holds jump button
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1);
-        bool grounded = hit.collider != null;
-        if(Input.GetMouseButtonDown(0) && grounded)
+        //Detecting Ground
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundMask);
+        grounded = hit.collider != null;
+
+        //Jumping
+        if(Input.GetMouseButtonDown(0))
         {
-            Vector2 velocity = new Vector2(0, jumpForce);
-            rb.velocity = velocity;
+            if(grounded)
+            {
+                doubleJumped = false;
+                Vector2 velocity = new Vector2(0, jumpForce);
+                rb.velocity = velocity;
+            }
+            else if (!doubleJumped)
+            {
+                doubleJumped = true;
+                Vector2 velocity = new Vector2(0, jumpForce);
+                rb.velocity = velocity;
+            }
         }
+
+        jumpHeld = Input.GetMouseButton(0);
+    }
+
+    private void FixedUpdate()
+    {
+        //Falling
+        if (jumpHeld && rb.velocity.y < 0)
+        {
+            Vector2 force = new Vector2(0, pushForce * Mathf.Abs(rb.velocity.y));
+            rb.AddForce(force);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (boxCollider == null) return; //Guard clause if boxCld is null
 
         //Raycast debug
         Color c = grounded ? Color.green : Color.red;
-        Debug.DrawRay(transform.position, Vector2.down * 1, c);
+        Gizmos.color = c;
+        Gizmos.DrawWireCube(transform.position + Vector3.down * 0.1f, boxCollider.bounds.size);
     }
 }
